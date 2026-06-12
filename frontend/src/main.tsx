7070359c64ef8b1688750564ed5f69e4343afd58
swaroop <swaroop.chitturi@deeptaai.com>
2026-06-12 19:57:20 +0530

    feat: add new UI components and configuration for Tailwind CSS and TanStack Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Providers from "@/components/providers";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const targetElement = document.querySelector("#root") as HTMLElement;
+const rootElement = createRoot(targetElement);
+
+rootElement.render(<Providers />);
